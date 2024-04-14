50e9e416b2ec018496c39bef27f20bc76a64d5a3
JIBSIL <tech101file@gmail.com>
2024-04-14 17:41:02 -0400

    bugfixes, cleaning up imports, making lightning training work

--- src/tests/lightning_trainer.py
+++ src/tests/lightning_trainer.py
@@ -1,19 +1,21 @@
-import numpy as np
-import pandas as pd
-import copy
-from pathlib import Path
+# import numpy as np
+# import pandas as pd
+# import copy
+# from pathlib import Path
 import torch
 import torch.nn as nn
 
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
-from pytorch_lightning.loggers import TensorBoardLogger
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import EarlyStopping, LearningRateMonitor
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.tuner import Tuner
+from lightning.pytorch import Trainer
 
 import pytorch_forecasting
-from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
-from pytorch_forecasting.data import GroupNormalizer
-from pytorch_forecasting.metrics import SMAPE, PoissonLoss, QuantileLoss
-from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
+# from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
+from pytorch_forecasting import TemporalFusionTransformer, TimeSeriesDataSet
+# from pytorch_forecasting.data import GroupNormalizer
+# from pytorch_forecasting.metrics import SMAPE, PoissonLoss, QuantileLoss
 
 import matplotlib.pyplot as plt
 
@@ -44,16 +46,15 @@ def prepare_and_train(df_train):
         allow_missing_timesteps=True
     )
     
-    validation = TimeSeriesDataSet.from_dataset(training, df_train, predict=True, stop_randomization=True)
+    validation = TimeSeriesDataSet.from_dataset(training, df_train, stop_randomization=True)
 
     batch_size = 128
-    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=0)
-    val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=0)
+    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size)
+    val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size)# * 10)
     
     # configure network and trainer
     pl.seed_everything(42)
-    trainer = pl.Trainer(
-        gpus=1,
+    trainer = Trainer(
         # clipping gradients is a hyperparameter and important to prevent divergance
         # of the gradient for recurrent neural networks
         gradient_clip_val=0.1,
@@ -77,17 +78,18 @@ def prepare_and_train(df_train):
     print(f"Number of parameters in network: {tft.size()/1e3:.1f}k")
     
     # find optimal learning rate
-    res = trainer.tuner.lr_find(
-        tft,
-        train_dataloader=train_dataloader,
+    tuner = Tuner(trainer)
+    res = tuner.lr_find(
+        model=tft,
+        train_dataloaders=train_dataloader,
         val_dataloaders=val_dataloader,
         max_lr=0.1,
         min_lr=1e-7,
     )
 
     print(f"suggested learning rate: {res.suggestion()}")
-    fig = res.plot(show=True, suggest=True)
-    fig.show()
+    # fig = res.plot(show=True, suggest=True)
+    # fig.show()
     
     early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-7, patience=10, verbose=False, mode="min")
     lr_logger = LearningRateMonitor()  
@@ -95,8 +97,7 @@ def prepare_and_train(df_train):
 
     trainer = pl.Trainer(
         max_epochs=30,
-        gpus=1,
-        weights_summary="top",
+        enable_model_summary=True,
         gradient_clip_val=0.1,
         limit_train_batches=30,  
         callbacks=[lr_logger, early_stop_callback],
@@ -113,14 +114,14 @@ def prepare_and_train(df_train):
         hidden_continuous_size=8,
         output_size=1, 
         loss=pytorch_forecasting.metrics.RMSE(),
-        log_interval=10,  
+        log_interval=0,  
         reduce_on_plateau_patience=4,
     )
     print(f"Number of parameters in network: {tft.size()/1e3:.1f}k")
     
     trainer.fit(
         tft,
-        train_dataloader=train_dataloader,
+        train_dataloaders=train_dataloader,
         val_dataloaders=val_dataloader,
     )
     
@@ -128,8 +129,8 @@ def prepare_and_train(df_train):
     best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
     
     # calcualte root mean squared error on validation set
-    actuals = torch.cat([y[0] for x, y in iter(val_dataloader)])
+    actuals = torch.cat([y[0] for x, y in iter(val_dataloader)]).to("cuda")
     val_predictions = best_tft.predict(val_dataloader)
     
     criterion = nn.MSELoss()
-    print(torch.sqrt(criterion(actuals,val_predictions)))
+    print(torch.sqrt(criterion(actuals, val_predictions)))
